d99eb27eea655e094ee1993ed15728d8b51c0952
dangalg <dangalg@gmail.com>
2022-04-19 23:17:46 +0300

    Added led testing

--- testing_leds/testing_leds.ino
+++ testing_leds/testing_leds.ino
@@ -1,8 +1,9 @@
-#define LED 13  // The pin the LED is connected to
+#define LED 12  // The pin the LED is connected to
 void setup() {
   pinMode(LED, OUTPUT); // Declare the LED as an output
 }
 
 void loop() {
   digitalWrite(LED, HIGH); // Turn the LED on
+  digitalWrite(13, LOW); // Turn the LED on
 }
